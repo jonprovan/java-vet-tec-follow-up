4d721d154ce4d51e6a6da345393ecf07066365a5
Jon Provan <jprovan@skillstorm.com>
2023-06-27 16:37:55 -0400

    Week 18, Day 2 Afternoon Fix

--- week-18/day-2/w18-d2-exercises/src/com/skillstorm/NestedLoopsAndLabels.java
+++ week-18/day-2/w18-d2-exercises/src/com/skillstorm/NestedLoopsAndLabels.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class NestedLoopsAndLablels {
+public class NestedLoopsAndLabels {
 
 	public static void main(String[] args) {
 
